6a7bddc32df167fa53732a9e04a6a0915e4bad91
ziphyrr <113685833+ziphyrr@users.noreply.github.com>
2022-11-26 01:33:21 +0300

    test app modified

--- online_inference/main.py
+++ online_inference/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get('/asd')
+async def root():
+    return {"message": "Hello worldasd!"}
+
 @app.get('/')
 async def root():
     return {"message": "Hello world!"}
+
+
